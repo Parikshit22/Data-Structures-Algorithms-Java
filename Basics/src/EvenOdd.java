ff7c51f4fdef04222a232a741e1dfa443444baa0
Parikshit22 <parikshitagarwal40@gmail.com>
2020-12-12 08:45:59 +0530

    Added EvenOdd

---
+++ Basics/src/EvenOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		if(n%2==0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+	}
+
+}
